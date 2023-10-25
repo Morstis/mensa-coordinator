56b1e2613d84abc69d87fc81c7dd97f3ad56e9e6
Lucas Wiese <lucas.wiese@gmx.de>
2023-10-25 02:30:22 +0200

    loaded osm and indoorequal with ol

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Map from 'ol/Map';
+	import View from 'ol/View';
+	import TileLayer from 'ol/layer/Tile';
+	import { fromLonLat, toLonLat } from 'ol/proj';
+	import OSM from 'ol/source/OSM';
+	import TileJSON from 'ol/source/TileJSON';
+	import { add } from 'ol/coordinate';
+	import IndoorEqual, { LevelControl } from 'openlayers-indoorequal';
+	import { onMount } from 'svelte';
+	import { coordinateRelationship } from 'ol/extent';
+
+	const apiKey = 'iek_exsKG2tSMNmq2Yy3J9kS3D3AKj4R';
+	onMount(() => {
+		const osmLayer = new TileLayer({
+			source: new OSM()
+		});
+		let map = new Map({
+			target: mapContainer,
+			layers: [osmLayer],
+			view: new View({
+				center: fromLonLat([8.41728, 49.011819]),
+				zoom: 18.47
+			})
+		});
+		const indoorEqual = new IndoorEqual(map, { apiKey });
+		const control = new LevelControl(indoorEqual);
+		map.addControl(control);
+	});
+
+	let mapContainer: HTMLDivElement;
+</script>
+
+<div bind:this={mapContainer} />
+
+<style>
+	div {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
